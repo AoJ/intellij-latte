2fb6c0b006b7f643363e7a13a04c8a8fce21e872
Jan Dolecek <juzna.cz@gmail.com>
2012-01-26 02:12:07 +0100

    simple keyword completion

--- src/cz/juzna/latte/completion/KeywordCompletionProvider.java
+++ src/cz/juzna/latte/completion/KeywordCompletionProvider.java
@@ -5,6 +5,7 @@ import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.util.ProcessingContext;
 import cz.juzna.latte.lexer.LatteTokenTypes;
 import org.jetbrains.annotations.NotNull;
@@ -17,10 +18,30 @@ import java.util.List;
  */
 public class KeywordCompletionProvider extends CompletionProvider<CompletionParameters> {
     // constant lists
-    private static final String[] KEYWORDS = { "for", "foreach", "/for", "if", "else", "/if", "link", "plink" };
-    private static final String[] FILTERS = { "date", "format", "replace", "url_encode", "json_encode", "title", "capitalize", "upper", "lower", "striptags", "join", "reverse", "length", "sort", "default", "keys", "escape", "raw", "merge" };
+    private static final String[] KEYWORDS = {
+		    // CoreMacros.php
+		    "if", "ifset", "for", "foreach", "while", "first", "last", "sep", "class", "attr", "captute",
+		    "var", "default", "dump", "debugbreak", "l", "r",
 
-    // CompletionResultSet wants list of LookupElements
+		    // FormMacros.php
+		    "form", "input", "label", "formContainer",
+
+		    // UIMacros.php
+		    "include", "extends", "block", "define", "snippet", "widget", "control", "href", "link", "plink", "contentType", "status",
+    };
+
+	private static final String[] FILTERS = {
+			// static
+			"normalize", "toascii", "webalize", "truncate", "lower", "upper", "firstupper", "capitalize", "trim", "padleft", "padright",
+			"replacere", "url", "striptags", "nl2br", "substr", "repeat", "implode", "number",
+
+			// methods in Helpers.php
+			"espaceHtml", "escapeHtmlComment", "escapeXML", "escapeCss", "escapeJs", "strip", "indent", "date", "bytes",
+			"length", "replace", "dataStream", "null",
+	};
+
+
+	// CompletionResultSet wants list of LookupElements
     private List<LookupElementBuilder> KEYWORD_LOOKUPS = new ArrayList();
     private List<LookupElementBuilder> FILTER_LOOKUPS = new ArrayList();
 
@@ -37,9 +58,19 @@ public class KeywordCompletionProvider extends CompletionProvider<CompletionPara
                                   ProcessingContext ctx,
                                   @NotNull CompletionResultSet results) {
 
-        PsiElement currElement = params.getPosition().getOriginalElement();
-        if(currElement.getNode().getElementType() == LatteTokenTypes.MACRO_NAME) {
+	    // Keywords
+        PsiElement curr = params.getPosition().getOriginalElement();
+        if(curr.getNode().getElementType() == LatteTokenTypes.MACRO_NAME) {
             for(LookupElementBuilder x: KEYWORD_LOOKUPS) results.addElement(x);
+	        results.stopHere();
         }
+
+	    // Modifiers (if after pipe)
+	    PsiElement prev = curr.getPrevSibling();
+	    if(prev != null && prev instanceof PsiWhiteSpace) prev = prev.getPrevSibling();
+	    if(prev != null && prev.getNode().getElementType() == LatteTokenTypes.MODIFIER) {
+		    for(LookupElementBuilder x: FILTER_LOOKUPS) results.addElement(x);
+		    results.stopHere();
+	    }
     }
 }
